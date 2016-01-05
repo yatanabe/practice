16f2a8dd59a303779d993e11a290c919f7a049e6
watanabe <watanabe@lassic.co.jp>
2016-01-05 23:26:26 +0900

    サンプル

---
+++ 06/s04/menu_items.js
@@ -0,0 +1,26 @@
+// menu_items.js file
+// comments block here
+
+var MENU_ITEMS =[
+	["HOME", "link.htm"],
+	["COLUMN", "link.htm",
+		["緑のコラム", "link.htm"],
+		["花のコラム", "link.htm"],
+		["公園ガイド", "link.htm"],
+		["お散歩ガイド", "link.htm"]
+	],
+	["PROFILE", "link.htm",
+		["スタッフA", "link.htm"],
+		["スタッフB", "link.htm"],
+		["スタッフC", "link.htm"]
+	],
+	["LINK", "link.htm",
+		["園芸店リンク", "link.htm"],
+		["お気に入り", "link.htm",
+			["便利なサイト", "link.htm"],
+			["ニュース／新聞", "link.htm"],
+			["お友達のページ", "link.htm"]
+		]
+	],
+	["CONTACT", "link.htm"]
+];
